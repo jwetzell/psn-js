ae962e2cf0812b309a8ca50550179f4c2ad0c11b
Joel Wetzell <jwetzell@yahoo.com>
2023-09-13 15:59:45 -0500

    export tracker class

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Decoder = require('./decoder.js');
 const Encoder = require('./encoder.js');
+const Tracker = require('./models/tracker.js');
 
 module.exports = {
   Encoder,
   Decoder,
+  Tracker,
 };
